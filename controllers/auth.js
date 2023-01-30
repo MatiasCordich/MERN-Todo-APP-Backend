e6250827c871bcbe32ef8153d7cde52523497037
MatiasCordich <m.scordich@gmail.com>
2023-01-30 00:49:42 -0300

    cookies modified

--- controllers/auth.js
+++ controllers/auth.js
@@ -80,7 +80,9 @@ const loginCtrl = async (req, res) => {
         })
 
         return res.cookie('access_token', token, {
-            httpOnly: false
+            httpOnly: false,
+            sameSite: 'none',
+            secure: process.env.NODE_ENV === 'production'
         }).status(200).send({ msg: "LOGIN_SUCCESS", status: true })
 
 
